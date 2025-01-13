b21c93d1d2a5d72c110a3aa42e186b7c44894b7f
GuiTaekM <guitaekm@gmail.com>
2025-01-13 19:16:51 +0100

    initial commit

---
+++ common/src/main/java/com/gmail/guitaekm/scriptor_unique_grammar/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.gmail.guitaekm.scriptor_unique_grammar;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "scriptor_unique_grammar";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/com/gmail/guitaekm/scriptor_unique_grammar/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package com.gmail.guitaekm.scriptor_unique_grammar.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import com.gmail.guitaekm.scriptor_unique_grammar.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
